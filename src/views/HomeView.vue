79da3671dc20d265e9a08230bdf9d1f0387ea209
Bob Summerwill <bob@summerwill.net>
2023-01-11 15:55:09 -0800

    Added suggestions
    * Added suggestions nav
    * Added body of text for suggestions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -35,6 +35,11 @@
               class="hidden lg:block font-roboto font-semibold text-textColor text-lg my-auto navItems hover:text-headingsColor transition duration-700"
               >{{ $t("requirements.nav-heading") }}</a
             >
+            <a
+              href="#suggestions"
+              class="hidden lg:block font-roboto font-semibold text-textColor text-lg my-auto navItems hover:text-headingsColor transition duration-700"
+              >{{ $t("suggestions.nav-heading") }}</a
+            >
 
             <a
               href=""
@@ -143,6 +148,15 @@
           <p class="font-notoSans text-textColor text-base font-normal pb-4">
             {{ $t("requirements.publiclyVisible.nav-heading") }}
           </p>
+
+          <!-- Suggestions mobile nav -->
+          <a
+            v-on:click="closeNav()"
+            href="#suggestions"
+            class="pt-6 font-notoSans text-headingsColor text-2xl font-bold leading-rightHeadingsDt"
+          >
+            {{ $t("suggestions.nav-heading") }}
+          </a>
 
         </div>
       </section>
@@ -315,6 +329,17 @@
                   {{ $t("requirements.publiclyVisible.nav-heading") }}
                 </a>
               </div>
+
+              <!-- Suggestions desktop navigation -->
+              <div class="text-headingsColor pt-8">
+                <a
+                  class="font-notoSans text-headingsColor text-2xl font-bold leading-rightHeadingsDt"
+                  href="#suggestions"
+                >
+                  {{ $t("suggestions.nav-heading") }}
+                </a>
+              </div>
+
 
             </div>
           </div>
@@ -682,6 +707,49 @@
               </p>
             </div>
 
+            <!-- Suggestions -->
+            <a id="suggestions"/>
+            <div class="border-b-2 pb-16"></div>
+            <div class="pt-16"></div>
+            <div class="text-headingsColor">
+              <p
+                class="font-notoSans text-headingsColor text-thirtyTwo md:text-fourty leading-rightHeadingsDt font-bold"
+              >
+                {{ $t("suggestions.heading") }}
+              </p>
+            </div>
+            <div class="text-headingsColor pt-12">
+              <p
+                class="font-roboto text-textColor text-base md:text-heroTextMobile md:leading-thirtyThree righttSecText"
+              >
+                {{ $t("suggestions.body.p1") }}
+              </p>
+            </div>
+            <div class="text-headingsColor pt-12">
+              <p
+                class="font-roboto text-textColor text-base md:text-heroTextMobile md:leading-thirtyThree righttSecText"
+              >
+                - {{ $t("suggestions.body.bullets.b1") }} <br/>
+                - {{ $t("suggestions.body.bullets.b2") }} <br/>
+                - {{ $t("suggestions.body.bullets.b3") }} <br/>
+                - {{ $t("suggestions.body.bullets.b4") }} <br/>
+                - {{ $t("suggestions.body.bullets.b5") }} <br/>
+                - {{ $t("suggestions.body.bullets.b6") }} <br/>
+                - {{ $t("suggestions.body.bullets.b7") }} <br/>
+                - {{ $t("suggestions.body.bullets.b8") }} <br/>
+                - {{ $t("suggestions.body.bullets.b9") }} <br/>
+                - {{ $t("suggestions.body.bullets.b10") }} <br/>
+                - {{ $t("suggestions.body.bullets.b11") }} <br/>
+                - {{ $t("suggestions.body.bullets.b12") }} <br/>
+                - {{ $t("suggestions.body.bullets.b13") }} <br/>
+                - {{ $t("suggestions.body.bullets.b14") }} <br/>
+                - {{ $t("suggestions.body.bullets.b15") }} <br/>
+                - {{ $t("suggestions.body.bullets.b16") }} <br/>
+                - {{ $t("suggestions.body.bullets.b17") }} <br/>
+              </p>
+            </div>
+
+            <!-- Application form at bottom of page -->
             <a
               href=""
               class="mx-auto md:mx-0 px-5 py-3 rounded text-headingsColor bg-gradient-to-r from-[#133706] to-[#53EB45] font-notoSans w-rightSecButton text-xl leading-twentyFour hidden lg:flex mt-8 hover:scale-110 transition duration-300 ease-in-out"
