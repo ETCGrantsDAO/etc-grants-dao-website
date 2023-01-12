d838e2319cea0211ff2b2ee7f89174dd2a41e9bc
SPTechnology <ScottPeacockTechnology@gmail.com>
2023-01-12 18:50:30 +0500

    created components for heading, paragraph and table

--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -1,81 +1,77 @@
+
+
 <template>
-    <table
+  <table
     class="rounded-[5px] overflow-hidden border-t-4 border-b-[12px] border-l-4 border-r-4 text-base mx-auto sm:w-tableDesktopWidth ml-tableMobile mr-tableMobile xs:ml-0 xs:mr-0 pr-9 bg-tableBackground font-dmSans md:text-lg rounded mt-12 md:border-r-fourty md:border-l-fourty">
     <thead>
       <tr>
-        <th
-          class="overflow-hidden text-left border-b-[2.5px] border-solid border-tableHBorder pl-1 pt-2 font-notoSans leading-8">
-          {{ $t("about.table.tableHeading1") }}
-        </th>
-        <th
-          class="overflow-hidden text-left border-b-[2.5px] border-solid border-tableHBorder pl-1 pt-2 font-notoSans leading-8">
-          {{ $t("about.table.tableHeading2") }}
-        </th>
-        <th
-          class="overflow-hidden text-left border-b-[2.5px] border-solid border-tableHBorder pl-1 pt-2 font-notoSans leading-8">
-          {{ $t("about.table.tableHeading3") }}
-        </th>
-        <th
-          class="overflow-hidden text-left border-b-[2.5px] border-solid border-tableHBorder pl-1 pt-2 font-notoSans leading-8">
-          {{ $t("about.table.tableHeading4") }}
-        </th>
+        <TH text="about.table.tableHeading1" />
+        <TH text="about.table.tableHeading2" />
+        <TH text="about.table.tableHeading3" />
+        <TH text="about.table.tableHeading4" />
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol1-1") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol2-1") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol3-1") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol4-1") }}</td>
+        <TD text="about.table.tableCol1-1" />
+        <TD text="about.table.tableCol2-1" />
+        <TD text="about.table.tableCol3-1" />
+        <TD text="about.table.tableCol4-1" />
       </tr>
       <tr>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol1-2") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol2-2") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol3-2") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol4-2") }}</td>
+        <TD text="about.table.tableCol1-2" />
+        <TD text="about.table.tableCol2-2" />
+        <TD text="about.table.tableCol3-2" />
+        <TD text="about.table.tableCol4-4" />
       </tr>
       <tr>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol1-3") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol2-3") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol3-3") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol4-3") }}</td>
+        <TD text="about.table.tableCol1-3" />
+        <TD text="about.table.tableCol2-3" />
+        <TD text="about.table.tableCol3-3" />
+        <TD text="about.table.tableCol4-3" />
       </tr>
       <tr class="bg-parrotGreen font-bold">
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol1-4") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol2-4") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol3-4") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol4-4") }}</td>
+        <TD text="about.table.tableCol1-4" />
+        <TD text="about.table.tableCol2-4" />
+        <TD text="about.table.tableCol3-4" />
+        <TD text="about.table.tableCol4-4" />
       </tr>
       <tr>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol1-5") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol2-5") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol3-5") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol4-5") }}</td>
+        <TD text="about.table.tableCol1-5" />
+        <TD text="about.table.tableCol2-5" />
+        <TD text="about.table.tableCol3-5" />
+        <TD text="about.table.tableCol4-5" />
       </tr>
       <tr>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol1-6") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol2-6") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol3-6") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol4-6") }}</td>
+        <TD text="about.table.tableCol1-6" />
+        <TD text="about.table.tableCol2-6" />
+        <TD text="about.table.tableCol3-6" />
+        <TD text="about.table.tableCol4-6" />
       </tr>
       <tr>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol1-7") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol2-7") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol3-7") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol4-7") }}</td>
+        <TD text="about.table.tableCol1-7" />
+        <TD text="about.table.tableCol2-7" />
+        <TD text="about.table.tableCol3-7" />
+        <TD text="about.table.tableCol4-7" />
       </tr>
       <tr>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol1-8") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol2-8") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol3-8") }}</td>
-        <td class="pl-1 pt-2 text-left">{{ $t("about.table.tableCol4-8") }}</td>
+        <TD text="about.table.tableCol1-8" />
+        <TD text="about.table.tableCol2-8" />
+        <TD text="about.table.tableCol3-8" />
+        <TD text="about.table.tableCol4-8" />
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import TH from './th.vue'
+import TD from './td.vue'
 export default {
-    name: "Table"
+  name: "Table",
+  components: {
+    TH,
+    TD
+  },
 }
 </script>
---
+++ src/components/Table/th.vue
@@ -0,0 +1,13 @@
+<template>
+    <th
+    class="overflow-hidden text-left border-b-[2.5px] border-solid border-tableHBorder pl-1 pt-2 font-notoSans leading-8">
+    
+    {{ $t(text) }}
+  </th>
+</template>
+<script>
+export default {
+    name: "TH",
+    props: ['text']
+}
+</script>
