c2c8bc3168100fd888a30d92576317a4caab0c4a
Bob Summerwill <bob@summerwill.net>
2023-01-12 16:13:02 -0800

    Fixed some stray relative paths

--- src/components/Hero/index.vue
+++ src/components/Hero/index.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="hero">
         <div
-          class="bg-[url('../images/heroBgMobile.svg')] grid-cols-1 md:mt-14 heroSection grid md:grid-cols-2 bg-no-repeat bg-contain md:bg-cover min-h-heroHeight sm:bg-[url('../images/heroBg.svg')]">
+          class="bg-[url('@/images/heroBgMobile.svg')] grid-cols-1 md:mt-14 heroSection grid md:grid-cols-2 bg-no-repeat bg-contain md:bg-cover min-h-heroHeight sm:bg-[url('@/images/heroBg.svg')]">
           <div
             class="my-auto mx-auto md:mx-0 md:my-0 text-center md:text-left container md:pl-8 xl:pl-28 md:pr-0 pt-11 content-end">
             <p
@@ -15,7 +15,7 @@
             <a href="https://airtable.com/shr3VMqyLd1HX5ws4"
               class="text-xl font-bold md:text-heroTextMobile mx-auto md:mx-0 px-5 py-3 rounded text-headingsColor bg-gradient-to-r from-[#133706] to-[#53EB45] font-notoSans w-heroButtonMobileWidth md:w-heroButtonDesktopWidth heroButton flex hover:scale-110 transition duration-300 ease-in-out">
               <p>{{ $t("etc-grants-dao.apply-now") }}</p>
-              <img src="../../images/applyButtonIcon.png"
+              <img src="@/images/applyButtonIcon.png"
                 class="max-w-heroBtnIcon max-h-heroBtnIcon ml-heroBtnIcon mt-heroBtnIconA" />
             </a>
           </div>
