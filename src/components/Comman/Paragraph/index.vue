8204e24866b5750192b963a1a2730ef3783ca423
Bob Summerwill <bob@summerwill.net>
2023-01-13 16:31:05 -0800

    Messing around
    The translation supports nested strings, and I can do a horrible substitution like this with hardcoded URLs in Paragraph, but they all end up inside 'p' elements where we actually need to be generating anchors.
    
    Looks to me like we are going to need to be using Vue components in a strong form for this to be possible, not just using Vue components to aggregate raw HTML with string substitutions.

--- src/components/Comman/Paragraph/index.vue
+++ src/components/Comman/Paragraph/index.vue
@@ -2,7 +2,12 @@
     <div class="pt-12">
         <p
           class="font-roboto text-textColor text-base md:text-heroTextMobile md:leading-thirtyThree righttSecText">
-          {{ $t(text) }}
+          {{
+            $t(text, {
+                link1: 'https://etccooperative.org/posts/2022-11-16-outline-of-xmei-lins-and-bob-summerwills-speeches-at-bitmain-wdsm-global-2022-en">',
+                link2: 'LINK2'
+            })
+            }}
         </p>
     </div>
 </template>
