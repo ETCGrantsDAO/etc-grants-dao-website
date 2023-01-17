693d4bb68304616d7e61bf78d630f0cc42081e10
Bob Summerwill <bob@summerwill.net>
2023-01-17 12:37:32 -0800

    Using parrot green, and adding that to the sub-sub-headers in ETC Grants DAO (#106)
    And removed left over About table.

--- src/components/Common/SubHeading/index.vue
+++ src/components/Common/SubHeading/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-headingsColor pt-6">
         <h2
-            class="font-notoSans text-headingsColor text-twentyFour leading-thirtySix md:text-thirtyTwo md:leading-rightHeadingsDt rightSecHeading font-bold font-green">
+            class="font-notoSans text-headingsColor text-twentyFour leading-thirtySix md:text-thirtyTwo md:leading-rightHeadingsDt rightSecHeading font-bold font-parrotGreen">
             {{ $t(title) }}
         </h2>
     </div>
