86cc04eea9f791d66774e67112796ded9bb55195
Bob Summerwill <bob@summerwill.net>
2023-01-18 14:24:06 -0800

    Fix two more typos (#127)

--- src/components/Hero/index.vue
+++ src/components/Hero/index.vue
@@ -12,7 +12,7 @@
               class="text-heroTextMobile md:text-heroTextDesktop text-headingsColor leading-heroText pt-1 pb-6 font-dmSans pl-8 pr-8 md:pl-0 md:pr-0">
               {{ $t("etc-grants-dao.description") }}
             </p>
-            <a href="https://airtable.com/shr3VMqyLd1HX5ws4?prefill_Form%20Language=English?prefill_Form%20Language=English"
+            <a href="https://airtable.com/shr3VMqyLd1HX5ws4?prefill_Form%20Language=English"
               class="text-xl font-bold md:text-heroTextMobile mx-auto md:mx-0 px-5 py-3 rounded text-headingsColor bg-gradient-to-r from-[#133706] to-[#53EB45] font-notoSans w-heroButtonMobileWidth md:w-heroButtonDesktopWidth heroButton flex hover:scale-110 transition duration-300 ease-in-out">
               <p>{{ $t("etc-grants-dao.apply-now-text") }}</p>
               <img src="@/images/applyButtonIcon.png"
