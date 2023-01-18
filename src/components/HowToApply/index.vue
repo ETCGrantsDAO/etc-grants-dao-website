c3098174a6b7887ced5c5c90cebfa3e0980b2bc5
Bob Summerwill <bob@summerwill.net>
2023-01-17 20:23:51 -0800

    Remove old table
    Switched to text for WeChat link.
    Stripped out example link code for Chinese JSON.

--- src/components/HowToApply/index.vue
+++ src/components/HowToApply/index.vue
@@ -18,39 +18,6 @@
         <section id="howToApply.categories" class="scroll-element">
             <div class="pt-6"></div>
             <img src="@/images/grant-categories.png"/>
-
-            <table
-            class="rounded-[5px] overflow-hidden border-t-4 border-b-[12px] border-l-4 border-r-4 text-base mx-auto ml-tableMobile mr-tableMobile xs:ml-0 xs:mr-0 pr-9 bg-tableBackground font-dmSans md:text-lg rounded mt-12 md:border-r-fourty md:border-l-fourty">
-                <thead>
-                    <tr>
-                        <TH text="howToApply.categories.table.tableHeading1" />
-                        <TH text="howToApply.categories.table.tableHeading2" />
-                        <TH text="howToApply.categories.table.tableHeading3" />
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <TD text="howToApply.categories.table.tableCol1-1" />
-                        <TD text="howToApply.categories.table.tableCol2-1" />
-                        <TD text="howToApply.categories.table.tableCol3-1" />
-                    </tr>
-                    <tr>
-                        <TD text="howToApply.categories.table.tableCol1-2" />
-                        <TD text="howToApply.categories.table.tableCol2-2" />
-                        <TD text="howToApply.categories.table.tableCol3-2" />
-                    </tr>
-                    <tr>
-                        <TD text="howToApply.categories.table.tableCol1-3" />
-                        <TD text="howToApply.categories.table.tableCol2-3" />
-                        <TD text="howToApply.categories.table.tableCol3-3" />
-                    </tr>
-                    <tr>
-                        <TD text="howToApply.categories.table.tableCol1-4" />
-                        <TD text="howToApply.categories.table.tableCol2-4" />
-                        <TD text="howToApply.categories.table.tableCol3-4" />
-                    </tr>
-                </tbody>
-            </table>
         </section>
 
         <!-- How to Apply / Suggestions -->
