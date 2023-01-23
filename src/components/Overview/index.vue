f28952a014178039cb376e892ede0440d897ea58
gd8097019 <102132498+gd8097019@users.noreply.github.com>
2023-01-23 20:31:12 +0300

    Convert Overview section to md file (#145)
    * Solution to hyperlink issue
    
    * Resolve issue with navigation, design and functionality
    
    * Resolve comments
    
    * Remove code for test purpose
    
    * Issue-128 remove additional padding
    
    * Added potential fixes for tasks 126 and 140
    
    * Remove excess padding for task 140 and added improvements for 96
    
    * md-file feature
    
    - add 'markdown-it' package to render md file
    - add 'markdown-it-attrs' package to render md file with class
    - add 'raw-loader' to import md file to vue.js
    - create 'en' and 'cn' md files for 'overview' section
    - detect change on language and md file

--- src/components/Overview/index.vue
+++ src/components/Overview/index.vue
@@ -1,50 +1,51 @@
 <template>
-
-    <!-- Overview-->
-    <section id="overview" class="parent-scroll-element">
-        <div class="pt-16"></div>
-        <MainHeading  title="overview.heading" />
-
-        <!-- Overview / ETC Grants DAO -->
-        <section id="overview.etc-grants-dao" class="scroll-element">
-            <SubHeading title="overview.etc-grants-dao.heading"/>
-            <Paragraph text="overview.etc-grants-dao.body.p1" />
-            <div v-for="(paragraph, index) in etcGrantsDaoList" class="pt-12">
-                <p class="font-roboto text-textColor text-base md:text-heroTextMobile md:leading-thirtyThree righttSecText font-parrotGreen font-bold">
-                    {{ $t(`overview.etc-grants-dao.body.p${index + 2}t`) }}
-                </p>
-                <p class="font-roboto text-textColor text-base md:text-heroTextMobile md:leading-thirtyThree righttSecText">
-                    {{ $t(`overview.etc-grants-dao.body.p${index + 2}`) }}
-                </p>
-            </div>
-        </section>
-
-       <!-- Overview / ETC Grants Program -->
-       <section id="overview.etc-grants-program" class="scroll-element">
-            <SubHeading title="overview.etc-grants-program.heading" />
-            <Paragraph text="overview.etc-grants-program.body.p1" />
-            <Paragraph text="overview.etc-grants-program.body.p2" />
-        </section>
-
-    </section>
-
+	<!-- Overview-->
+	<section id="overview" class="parent-scroll-element">
+		<div style="color: white" v-html="md(content)"></div>
+	</section>
 </template>
 <script>
-import MainHeading from '@/components/Common/MainHeading';
-import SubHeading from '@/components/Common/SubHeading';
-import Paragraph from '@/components/Common/Paragraph';
+import MainHeading from "@/components/Common/MainHeading";
+import SubHeading from "@/components/Common/SubHeading";
+import Paragraph from "@/components/Common/Paragraph";
 
 export default {
-    name: "Overview",
-    components: {
-        MainHeading,
-        SubHeading,
-        Paragraph
-    },
-    data() {
-        return {
-            etcGrantsDaoList: new Array(4)
-        }
-    }
-}
+	name: "Overview",
+	components: {
+		MainHeading,
+		SubHeading,
+		Paragraph,
+	},
+	watch: {
+		"$i18n.locale": function (newLang) {
+			this.getContent(newLang);
+		},
+	},
+	data() {
+		return {
+			etcGrantsDaoList: new Array(4),
+			content: "",
+		};
+	},
+	methods: {
+		getContent(lang) {
+			switch (lang) {
+				case "en":
+					import("@/md/overview.en.md").then((contentEN) => {
+						this.content = contentEN.default;
+					});
+					break;
+				case "cn":
+					import("@/md/overview.cn.md").then((contentCH) => {
+						this.content = contentCH.default;
+					});
+					break;
+			}
+		},
+	},
+	created() {
+		const lang = this.$i18n.locale;
+		this.getContent(lang);
+	},
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/tailwind.css'
-import i18n from './i18n'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/tailwind.css";
+import i18n from "./i18n";
+import markdown from "markdown-it";
+import markdownAttrs from "markdown-it-attrs";
 
-createApp(App).use(i18n).use(router).mount('#app')
+const mixins = {
+  methods: {
+    md: function (input) {
+      return markdown().use(markdownAttrs).render(input);
+    },
+  },
+};
+
+createApp(App).mixin(mixins).use(i18n).use(router).mount("#app");
