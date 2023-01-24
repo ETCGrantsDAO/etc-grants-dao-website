5a5864ff6c525654994a4b24049cd0d37554a5dc
Bob Summerwill <bob@summerwill.net>
2023-01-24 15:12:00 -0800

    Removed "Apply Now" at the bottom and added Green background for one in the header. (#157)

--- src/components/Navbar/NavBar/index.vue
+++ src/components/Navbar/NavBar/index.vue
@@ -31,7 +31,7 @@
           {{ $t("resources.nav-heading") }}
         </a>
         <a :href="$t('etc-grants-dao.apply-now-link')"
-          class="relative hidden lg:block font-notoSans font-semibold px-5 py-2 rounded text-headingsColor border-2 text-lg topApplyNow">
+          class="relative hidden lg:block font-notoSans font-semibold px-5 py-2 rounded text-headingsColor border-2 text-lg topApplyNow bg-gradient-to-r from-[#133706] to-[#53EB45]">
           {{ $t("etc-grants-dao.apply-now-text") }} 
         </a>
 
