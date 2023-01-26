f1ffb9e74b1a3ded8b6d02d8eea6efff2957183f
gd8097019 <gd8097019@gmail.com>
2023-01-26 12:03:46 +0300

    Move back button codes to component

--- src/views/WeChatView.vue
+++ src/views/WeChatView.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <div class="bg-background p-0 relative">
-      <div class="mt-5 ml-5 md:mt-10 md:ml-10">
-        <a href="/" class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-          <img class="mr-2" width="25" height="25" src="@/images/back.png" alt="back" />
-          <span>{{ $t('wechat.back-button') }}</span>
-        </a>
-      </div>
+      <Back/>
 
       <section id="content">
         <div class="p-6 md:pt-12 text-center">
@@ -20,12 +15,12 @@
   </div>
 </template>
 <script>
-import NavBar from "@/components/Navbar/NavBar"
+import Back from "@/components/Back"
 
 export default {
   name: "WeChatView",
   components: {
-    NavBar,
+    Back,
   }
 }
 </script>
