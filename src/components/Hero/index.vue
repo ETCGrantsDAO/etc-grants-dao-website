c55464e3740919a7e74b729ae6fb5fa54d5ef93d
gd8097019 <gd8097019@gmail.com>
2023-01-31 14:06:36 +0300

    "Hero" Apply Now button has disappeared on mobile. #256

--- src/components/Hero/index.vue
+++ src/components/Hero/index.vue
@@ -15,11 +15,13 @@
               <div class="pt-6"/>
               <img :src="$t('etc-grants-dao.key-dates-link')"/>
               <div class="pt-6"/>
-              <a 
-                :href="$t('etc-grants-dao.apply-now-link')"
-                class="text-xl font-bold md:text-heroTextMobile mx-auto md:mx-0 px-5 py-3 rounded text-headingsColor bg-gradient-to-r from-[#133706] to-[#53EB45] font-notoSans w-heroButtonMobileWidth md:w-heroButtonDesktopWidth heroButton flex hover:scale-110 transition duration-300 ease-in-out">
-                {{ $t("etc-grants-dao.apply-now-text") }}
-              </a>
+              <div class="inline-block font-semibold px-5 py-2 rounded text-headingsColor text-lg  bg-gradient-to-r from-[#133706] to-[#53EB45] w-heroButtonMobileWidth md:w-heroButtonDesktopWidth">
+                <a 
+                  :href="$t('etc-grants-dao.apply-now-link')"
+                  class="inline-block md:block lg:block font-notoSans w-16">
+                  {{ $t("etc-grants-dao.apply-now-text") }}
+                </a>
+              </div>
             </p>
           </div>
           <div class="hidden md:block"></div>
