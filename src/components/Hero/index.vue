ed0c20201f8c9ffd475e8446ea83152fe3b1a55e
gd8097019 <gd8097019@gmail.com>
2023-05-01 23:35:57 +0300

    change button color green to red

--- src/components/Hero/index.vue
+++ src/components/Hero/index.vue
@@ -18,7 +18,7 @@
               <div class="inline-block">
                 <a 
                   :href="$t('etc-grants-dao.apply-now-link')"
-                  class="relative lg:block font-notoSans font-semibold px-5 py-2 rounded text-headingsColor border-2 text-lg topApplyNow bg-gradient-to-r from-[#133706] to-[#53EB45]">
+                  class="relative lg:block font-notoSans font-semibold px-5 py-2 rounded text-headingsColor border-2 text-lg topApplyNow bg-gradient-to-r from-[#d92626] to-[#ff0606]">
                   {{ $t("etc-grants-dao.apply-now-text") }}
                 </a>
               </div>
--- src/components/Navbar/NavBar/index.vue
+++ src/components/Navbar/NavBar/index.vue
@@ -46,7 +46,7 @@
         </a> -->
 
         <a :href="$t('etc-grants-dao.apply-now-link')"
-          class="relative hidden lg:block font-notoSans font-semibold px-5 py-2 rounded text-headingsColor border-2 text-lg topApplyNow bg-gradient-to-r from-[#133706] to-[#53EB45]">
+          class="relative hidden lg:block font-notoSans font-semibold px-5 py-2 rounded text-headingsColor border-2 text-lg topApplyNow bg-gradient-to-r from-[#d92626] to-[#ff0606]">
           {{ $t("etc-grants-dao.apply-now-text") }}
         </a>
 
@@ -71,7 +71,7 @@
 
     <!-- Mobile navigation -->
     <a :href="$t('etc-grants-dao.apply-now-link')"
-      class="py-1 mt-11 text-black text-center bg-gradient-to-r from-[#133706] to-[#53EB45] font-notoSans text-base font-bold rounded">
+      class="py-1 mt-11 text-black text-center bg-gradient-to-r from-[#d92626] to-[#ff0606] font-notoSans text-base font-bold rounded">
       <span class="text-headingsColor applyNowBtn relative">
         {{ $t("etc-grants-dao.apply-now-text") }}
       </span>
