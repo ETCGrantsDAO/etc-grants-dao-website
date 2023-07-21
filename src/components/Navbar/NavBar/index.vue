76099e73dc98bfb7150ffd66538254ce5a4a3d1c
Andrew <andrewgdick@gmail.com>
2023-07-21 12:48:12 -0400

    Update index.vue

--- src/components/Navbar/NavBar/index.vue
+++ src/components/Navbar/NavBar/index.vue
@@ -46,7 +46,7 @@
         </a> -->
 
         <a :href="$t('etc-grants-dao.apply-now-link')"
-          class="relative hidden lg:block font-notoSans font-semibold px-5 py-2 rounded text-headingsColor border-2 text-lg topApplyNow bg-gradient-to-r from-[#133706] to-[#d92626]">
+          class="relative hidden lg:block font-notoSans font-semibold px-5 py-2 rounded text-headingsColor border-2 text-lg topApplyNow bg-gradient-to-r from-[#133706] to-[#53eb45]">
           {{ $t("etc-grants-dao.apply-now-text") }}
         </a>
 
